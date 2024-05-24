6ce653539229effac8899ac843e656a245e0febc
xristlamp <xristlamp@gmail.com>
2024-05-24 16:02:34 +0300

    aa

--- src/components/AddPet.js
+++ src/components/AddPet.js
@@ -31,7 +31,7 @@ function AddPetForm({ userId }) {
   return (
     <form onSubmit={handleSubmit}>
       <div>
-        <label>Name:</label>
+        <label>Namee:</label>
         <input type="text" name="name" value={petInfo.name} onChange={handleChange} />
       </div>
       <div>
